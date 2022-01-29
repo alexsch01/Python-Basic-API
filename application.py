382483578b10d3d49fb3b8bf7e6d6150276dff99
Alex Schwartz <alexschwartz01@gmail.com>
2022-01-29 12:08:16 -0500

    Make blog page with Bootstrap, HTML, CSS, Python, and Flask

--- application.py
+++ application.py
@@ -1,47 +1,29 @@
-from flask import Flask, request
-from flask_sqlalchemy import SQLAlchemy
-
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
-db = SQLAlchemy(app)
 
-class Item(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), unique=True, nullable=False)
-    description = db.Column(db.String(100))
-
+posts = [ 
+    {
+        'author': "Person1",
+        'title': 'Blog Post 1',
+        'content': 'First Post content',
+        'date': 'May 3, 2019'
+    },
+    {
+        'author': "Mary Jane",
+        'title': 'Blog Post 2',
+        'content': 'Second Post content',
+        'date': 'May 4, 2019'
+    }
+]
 
 @app.route('/')
-def index():
-    return "Hello World"
+@app.route('/home')
+def home():
+    return render_template('home.html',posts=posts)
 
-@app.route('/items')
-def get_items():
-    items = []
-    for item in Item.query.all():
-        items.append({'name': item.name,
-                      "description": item.description})
-    return {'items': items}
+@app.route('/about')
+def about():
+    return render_template('about.html',title="About")
 
-@app.route('/items/<id>')
-def get_item(id):
-    item = Item.query.get_or_404(id)
-    return {'name': item.name,
-            'description': item.description}
-
-@app.route('/items', methods=['POST'])
-def add_item():
-    item = Item(name=request.json["name"],
-                description=request.json["description"])
-    db.session.add(item)
-    db.session.commit()
-    return {'id': item.id}
-
-@app.route('/items/<id>', methods=["DELETE"])
-def delete_item(id):
-    item = Item.query.get(id)
-    if item is None:
-        return {"error": "not found"}
-    db.session.delete(item)
-    db.session.commit()
-    return {"message": "successful"}
+if __name__ == '__main__':
+    app.run(debug=True)
